cca275f3cd3133eca2f532424e7b07d98b665f01
yunjiwoo <dtroy371@gmail.com>
2022-07-30 19:43:39 +0900

    mistake code

--- src/programmers/Lotto.java
+++ src/programmers/Lotto.java
@@ -1,20 +1,31 @@
 package programmers;
 
+import java.util.Arrays;
+
 //로또 최고순위와 최저 순위
 
 /*
  * 0 의 개수에 따라 다르다
- * 0의 개수가
- * :
  * 
- *  0개 -> 그값이 최저이자 최고
- *  1개 -> 1,
- *  2개 -> 1,6
+ * 
+ * 
  * 
  */
 
 public class Lotto {
-	public int[] solution(int[] lottos, int[] win_nums)
+	   public static int num(int n)
+	    {
+	        if(n == 6) return 1;
+	        else if(n == 5) return 2;
+	        else if(n == 4) return 3;
+	        else if(n == 3) return 4;
+	        else if(n == 2) return 5;
+	        else if(n == 1) return 6;
+	        else if(n == 0) return 6;
+	        
+	        return n;
+	    }
+	public static int[] solution(int[] lottos, int[] win_nums)
 	{
 		int answer[] = new int[2];
 		int n = 0 ;
@@ -28,11 +39,12 @@ public class Lotto {
 		
 		for(int i = 0 ; i<lottos.length;i++)
 		{
-			if(lottos[i] == win_nums[i]) n++;
+			for(int j = 0 ; j<win_nums.length;j++)
+			if(lottos[i] == win_nums[j]) n++;
 		}
-		answer[0] = n+zero;
-		answer[1] = n;
-		
+		answer[0] = num(n+zero);
+		answer[1] = num(n);
+		System.out.println(Arrays.toString(answer));
 		return answer;
 	}
 	
@@ -41,6 +53,8 @@ public class Lotto {
 	{
 		int[] lotto = {44,1,0,0,31,25};
 		int[] win_lotos = {31,10,45,1,6,19};
+		
+		int[] ans = solution(lotto,win_lotos);
 	}
 
 }
